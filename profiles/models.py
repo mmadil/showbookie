929061f95efd1b7102a73dbf19be9d422c9d5db7
Mohammad Adil <mmadil_14@yahoo.com>
2013-05-21 12:49:16 +0530

    absolute_url fixed and added some more fields

--- profiles/models.py
+++ profiles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    profile_picture = models.ImageField(upload_to='profile_pictures/')
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
     bio = models.TextField(blank=True, default='')
     facebook = models.CharField('Facebook Username', max_length=30, blank=True, default='')
     twitter = models.CharField('Twitter Username', max_length=30, blank=True, default='')
