b255b80d9d51c989f6c9d38fb56f8b0452eb7311
Mohammad Adil <mmadil_14@yahoo.com>
2013-05-23 15:01:17 +0530

    working profile home page

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,13 @@
 from .models import UserProfile
 from django.contrib.auth.models import User
 
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect, Http404
+
 from django.views.generic import TemplateView
 
+
 class ProfileHomepageView(TemplateView):
-    model = UserProfile, User
+    context_object_name = 'userinfo'
+    model = User
     template_name = "profiles/profile_home.html"
