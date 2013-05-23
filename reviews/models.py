76ed0310854a3347ea8c01af844d5a43dce33f14
Mohammad Adil <mmadil_14@yahoo.com>
2013-05-23 12:17:40 +0530

    comments without pagination

--- reviews/models.py
+++ reviews/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.comments.models import Comment
 
-from movies.models import Movie
-
 class UserExperience(Comment):
     experience = models.CharField('User Experience', max_length=20)
 
