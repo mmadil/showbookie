bf92428b1deced1e03185ca5118346de89248caf
Mohammad Adil <mmadil_14@yahoo.com>
2013-05-26 02:27:37 +0530

    error not fixed

--- showbookie/settings/base.py
+++ showbookie/settings/base.py
@@ -6,9 +6,6 @@ here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 PROJECT_ROOT = here('..')
 root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
 
-
-from registration_defaults.settings import *
-
 # Django settings for showbookie project.
 
 DEBUG = False
