653049923f0ffa7b47a00cfc1d7bbf78145c27b7
Mohammad Adil <mmadil_14@yahoo.com>
2013-05-28 16:42:25 +0530

    comment will get its movie url

--- reviews/models.py
+++ reviews/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from movies.models import Movie
 from django.contrib.comments.models import Comment
 
 class UserExperience(Comment):
+    movie = models.ForeignKey(Movie)
     experience = models.CharField('User Experience', max_length=20)
 
 #    def __unicode__(self):
