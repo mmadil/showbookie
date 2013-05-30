d33452704b599dbe187b93d64e78e09e3f898088
Mohammad Adil <mmadil_14@yahoo.com>
2013-05-30 13:09:51 +0530

    removed context processor, was not needed

--- showbookie/settings/base.py
+++ showbookie/settings/base.py
@@ -70,7 +70,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
         'django.core.context_processors.media',
         'django.core.context_processors.request',
         'django.core.context_processors.static',
-        'showbookie.context_processors.this_week',
 )
 
 
