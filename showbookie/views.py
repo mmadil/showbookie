3a406516b42f43102528bc8783541f77cf539a37
Mohammad Adil <mmadil_14@yahoo.com>
2013-06-11 12:02:16 +0530

    search added and fixed

--- showbookie/views.py
+++ showbookie/views.py
@@ -6,5 +6,5 @@ from movies.models import Movie
 
 def IndexView(request):
 	now_showing = Movie.objects.filter(start_date__lte=datetime.date.today()).exclude(end_date__lte=datetime.date.today())[:5]
-	popular = Movie.objects.all().order_by('-user_rating_score')[:5]
+	popular = Movie.objects.filter(user_rating_votes__gte=1).order_by('-user_rating_score')[:5]
 	return render(request,'index.html',{'now_showing':now_showing, 'popular':popular})
